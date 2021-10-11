1a757866f62c069ba47af1c0a3d99842eafef88a
Atir Nayab <91016903+nayabatir1@users.noreply.github.com>
2021-10-11 08:12:44 +0530

    removed all prinln

--- twoSums.go
+++ twoSums.go
@@ -13,8 +13,6 @@ func twoSum(nums []int, target int) []int {
 				map_1[nums[j]] = j
 			}
 		}
-		fmt.Println(map_1, temp, map_1[temp])
-
 		if _, ok := map_1[temp]; ok {
 			return []int{i, map_1[temp]}
 		}
